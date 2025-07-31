28b4dea32c2dcd3d3ae61c8712e0daa7cc86c9f7
Abaddonq <sefasahin1762@gmail.com>
2025-07-31 14:13:05 +0300

    api routes wrapped with jwt

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { verifyJwt } from "@/lib/jwt";
+import { verifyJwt } from "@/lib/jwt"; 
 
 export function middleware(request: NextRequest) {
   if (
     request.nextUrl.pathname.startsWith("/admin") &&
-    !request.nextUrl.pathname.startsWith("/admin/login") &&
-    request.nextUrl.pathname.startsWith("/api/news")
+    !request.nextUrl.pathname.startsWith("/admin/login")
   ) {
-    const token = request.cookies.get("token")?.value;
-    const user = token ? verifyJwt(token) : null;
-    if (!user) {
+    const token = request.cookies.get("token")?.value; 
+    const user = token ? verifyJwt(token) : null; 
+    if (!user) { 
       return NextResponse.redirect(new URL("/admin/login", request.url));
     }
   }
-  return NextResponse.next();
+
+  
+  if (request.nextUrl.pathname.startsWith("/api")) {
+    const token = request.cookies.get("token")?.value;
+    const user = token ? verifyJwt(token) : null; 
+    if (!user) { 
+      return NextResponse.json({ error: "Yetkisiz erişim" }, { status: 401 });
+    }
+  }
+
+  return NextResponse.next(); 
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*", "/api/:path*"],
 };
