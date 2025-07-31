90288c25fb6f40300ec5f1f84a560570458e0e85
Abaddonq <sefasahin1762@gmail.com>
2025-07-31 14:23:59 +0300

    [fix]api routes wrapped with jwt

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,28 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { verifyJwt } from "@/lib/jwt"; 
+import { verifyJwt } from "@/lib/jwt";
 
 export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  const user = token ? verifyJwt(token) : null;
+
   if (
     request.nextUrl.pathname.startsWith("/admin") &&
     !request.nextUrl.pathname.startsWith("/admin/login")
   ) {
-    const token = request.cookies.get("token")?.value; 
-    const user = token ? verifyJwt(token) : null; 
     if (!user) { 
       return NextResponse.redirect(new URL("/admin/login", request.url));
     }
   }
 
-  
+
   if (request.nextUrl.pathname.startsWith("/api")) {
-    const token = request.cookies.get("token")?.value;
-    const user = token ? verifyJwt(token) : null; 
-    if (!user) { 
+    if (!user) {
       return NextResponse.json({ error: "Yetkisiz erişim" }, { status: 401 });
     }
   }
 
-  return NextResponse.next(); 
+  return NextResponse.next();
 }
 
 export const config = {
